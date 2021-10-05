a4c9f6ae6b43efd6de4e9bd679a25168cb1a0885
Dessinateur88 <65838177+Dessinateur88@users.noreply.github.com>
2021-10-04 21:53:09 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,6 @@
 <?php
 
-$mysqli=new mysqli ('localhost', 'root', '', 'fmm')
+/*$mysqli=new mysqli ('localhost', 'root', '', 'fmm')*/
+$mysqli=new mysqli ('fmm-test.mysql.database.azure.com', 'fmm@fmm-test','624UB8VWV61V7XS4$', 'fmm', '3306')
 
 ?>
